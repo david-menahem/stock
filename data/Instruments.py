9f08b255a602605d26145dadf2617cc1d0828f1c
david-menahem <dvmena39@gmail.com>
2024-03-24 00:12:28 +0200

    first

---
+++ data/Instruments.py
@@ -0,0 +1,23 @@
+# Dictionary with the 100 most traded instruments in the U.S.A.
+instruments = {
+    "Apple Inc.": "AAPL",
+    "Microsoft Corporation": "MSFT",
+    "Alphabet Inc.": "GOOGL",
+    "Amazon.com Inc.": "AMZN",
+    "Tesla Inc.": "TSLA",
+    "NVIDIA Corporation": "NVDA",
+    "Netflix Inc.": "NFLX",
+    "PayPal Holdings Inc.": "PYPL",
+    "Intel Corporation": "INTC",
+    "JPMorgan Chase & Co.": "JPM",
+    "Bank of America Corporation": "BAC",
+    "The Goldman Sachs Group Inc.": "GS",
+    "Walmart Inc.": "WMT",
+    "Visa Inc.": "V",
+    "The Walt Disney Company": "DIS",
+    "Cisco Systems Inc.": "CSCO",
+    "Verizon Communications Inc.": "VZ",
+    "Exxon Mobil Corporation": "XOM",
+    "Chevron Corporation": "CVX",
+    # Add more instruments as needed
+}
