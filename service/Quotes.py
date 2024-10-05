6433c1929df993f367b174a3533ce66920ec6267
david-menahem <dvmena39@gmail.com>
2024-10-06 02:59:37 +0300

    env vars

--- service/Quotes.py
+++ service/Quotes.py
@@ -4,15 +4,16 @@ from models.Quote import Quote
 
 
 class Quotes:
-    @staticmethod
-    def set_attr(time_frame):
+
+    def set_attr(self,time_frame):
         options = ('Yesterday', 'Last week', 'Last month', 'Last year')
         now = datetime.datetime.now()
         formatted_date = "%Y-%m-%d"
         gap = ""
         time_unit = ""
         start_date = ""
-        end_date = now - datetime.timedelta(days=2)
+        end_date = now - datetime.timedelta(days=1)
+        end_date = self.check_day_of_week(end_date)
         end_date = end_date.strftime(formatted_date)
 
         if time_frame == options[0]:
@@ -23,14 +24,18 @@ class Quotes:
             gap = "5"
             time_unit = "hour"
             start_date = now - datetime.timedelta(days=7)
+            start_date = self.check_day_of_week(start_date)
+
         elif time_frame == options[2]:
             gap = "1"
             time_unit = "day"
             start_date = now - datetime.timedelta(days=30)
+            start_date = self.check_day_of_week(start_date)
         elif time_frame == options[3]:
             gap = "3"
             time_unit = "week"
             start_date = now - datetime.timedelta(days=365)
+            start_date = self.check_day_of_week(start_date)
 
         if time_frame != options[0]:
             start_date = start_date.strftime(formatted_date)
@@ -65,4 +70,15 @@ class Quotes:
         except Exception as e:
             print(e)
 
+    @staticmethod
+    def check_day_of_week(day):
+        weekday = day.weekday()
+        valid_days = [0, 1, 2, 3, 4]
+        if weekday in valid_days:
+            adjust_days = 0
+        elif weekday == 5:
+            adjust_days = 1
+        else:
+            adjust_days = 2
 
+        return day - datetime.timedelta(days=adjust_days)
